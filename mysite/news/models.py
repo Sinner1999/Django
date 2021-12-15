09da26e2c36c9bba7a7ee37d7cee52907137cff7
sinner1999 <sinner1999@gmail.com>
2021-12-15 16:35:18 +0300

    123

---
+++ mysite/news/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    photo = models.ImageField(upload_to = 'photos/%Y/%m/%d/')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    is_published = models.BooleanField(default = True)
+    
+    def __str__(self):
+        return self.title
