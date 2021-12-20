fb9acb78df5d42e3e805b6c4e576e7f1fdc0e08d
sinner1999 <sinner1999@gmail.com>
2021-12-20 21:34:07 +0300

    123

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 # Create your views here.
 def index(request):
     news = News.objects.all()
+    category = Category.objects.all()
     content = {
         'news' : news,
-        'title': 'News list'
+        'title': 'News list',
+        'category': category,
     }
     
     return render(request, 'news/index.html', content)
 
+def get_category(request, category_id):
+    news = News.objects.filter(category_id = category_id)
+    category = Category.objects.all()
+    content = {
+        'news' : news,
+        'title': 'News list',
+        'category': category,
+        'id': category_id,
+    }
 
+    return render(request, 'news/index.html', content)
