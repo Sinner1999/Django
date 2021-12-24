bb312b29f0f83a059a2fa2459128a5de5c78ec13
sinner1999 <sinner1999@gmail.com>
2021-12-24 15:57:52 +0300

    123

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,9 +1,20 @@
 from django import forms
-from .models import Category
+from .models import News
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length = 150, label = "Назвавние")
-    content = forms.CharField(label = "Текст")
-    is_published = forms.BooleanField(label = "Опубликовано", initial = True)
-    category = forms.ModelChoiceField(queryset = Category.objects.all(), label = "Категория", empty_label = 'Choose category...')
+class NewsForm(forms.ModelForm):
+    
+    class Meta:
+        model = News
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs = {"class": "form-control"}),
+            'content': forms.Textarea(attrs = {"class": "form-control"}),
+            'is_published': forms.CheckboxInput(attrs = {"class": "form-check-input", "role": "switch"}),
+            'category': forms.Select(attrs = {"class": "form-control"}),
+        }
+    
+    # title = forms.CharField(max_length = 150, label = "Назвавние", widget = forms.TextInput(attrs = {"class": "form-control"}))
+    # content = forms.CharField(label = "Текст", widget = forms.Textarea(attrs = {"class": "form-control"}))
+    # is_published = forms.BooleanField(label = "Опубликовано", initial = True, widget = forms.CheckboxInput(attrs = {"class": "form-check-input", "role": "switch"}))
+    # category = forms.ModelChoiceField(queryset = Category.objects.all(), label = "Категория", empty_label = 'Choose category...', widget = forms.Select(attrs = {"class": "form-control"}))
     
